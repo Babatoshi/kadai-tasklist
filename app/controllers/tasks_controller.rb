a15147c3c566b13453c1c6be9fb8b78f9d2a6952
takatoshi.baba <ec2-user@ip-172-31-84-52.ec2.internal>
2019-12-16 14:49:09 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,17 +19,15 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Task' + @task.id.to_s + ' は正常に更新されました'
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Task' + @task.id.to_s + ' は正常に削除されました'
@@ -48,6 +46,10 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
